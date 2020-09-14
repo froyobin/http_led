1418f014c2760e780ab738509ba96ce77bc152d4
Ford Betelgeuse <ford.betelgeusedent@gmail.com>
2020-09-14 22:24:05 +1000

    add log in supervisor

--- binance_trading/sendprice.py
+++ binance_trading/sendprice.py
@@ -1,6 +1,7 @@
 from binance.client import Client
 import time
 import urllib3
+import logging
 
 api_key = "ckUg0iSmUGRkev85dM3MrstZcjPhDqvc0znL6PIaMmTf6QZl5ObKpWl0FO9wF1qZ"
 api_secret = "vAxzSEpi1QG5OVw67Ivp3IXa7AqDXO1NeW5Q2iHZIlK0XGDug41KX8NkEAGsNVxS"
@@ -8,15 +9,15 @@ api_secret = "vAxzSEpi1QG5OVw67Ivp3IXa7AqDXO1NeW5Q2iHZIlK0XGDug41KX8NkEAGsNVxS"
 
 def do_send(ip):
     binance_client = Client(api_key, api_secret)
+    logging.basicConfig(format='%(asctime)s %(message)s')
     while True:
         result = binance_client.get_avg_price(symbol='BTCBUSD')
         btcprice = result['price'].split(".")[0]
         send_data = ip + "/price=" + btcprice + "="
-        print(send_data)
         http = urllib3.PoolManager()
-        r = http.request('GET', send_data)
-        print(r.data)
-        time.sleep(5)
+        r = http.request('GET', send_data, timeout=10)
+        logging.warning('we send to the server %s' % send_data)
+        time.sleep(10)
 
 
 if __name__ == "__main__":
